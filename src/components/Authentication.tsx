6f90d6eb6a34b41933d6c03138961cc338dc3917
Arthur Police <arthurpolice1@gmail.com>
2023-02-07 12:05:17 -0400

    Removed rogue div

--- src/components/Authentication.tsx
+++ src/components/Authentication.tsx
@@ -108,7 +108,6 @@ export function AuthenticationForm(
               }
             />
           )}
-          <div>{usernameError}</div>
           <TextInput
             required
             label="Username"
